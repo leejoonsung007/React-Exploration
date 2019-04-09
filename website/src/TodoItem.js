79721824d4d166138f698239080a96c116d66aeb
leejoonsung <juncheng.li@ucdconnect.ie>
2019-04-09 23:46:55 +0100

    update
    09/04/2019

--- website/src/TodoItem.js
+++ website/src/TodoItem.js
@@ -21,7 +21,7 @@ class TodoItem extends Component{
 
 TodoItem.propTypes = {
   test: PropTypes.string.isRequired,
-  content: PropTypes.string,
+  content: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   deleteItem: PropTypes.func,
   index: PropTypes.number
 }
