a9a4d889070e344c0bb3819a3421448f4c594e63
leejoonsung <juncheng.li@ucdconnect.ie>
2019-04-15 00:24:17 +0100

    Redux exploration
    15/04/2019 React + Redux

--- website/src/TodoList.js
+++ website/src/TodoList.js
@@ -1,35 +1,65 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
-
-
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
-
+import store from './store';
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction } from './store/actionCreaters'
 
 class TodoList extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    store.subscribe(this.handleStoreChange);
+  }
 
    render() {
      return (
        <div>
          <div style={{marginTop: '10px', marginLeft: '10px'}}>
-           <Input placeholder='todo info' style={{width: '300px', marginRight: '10px'}}></Input>
-           <Button type="primary">Submit</Button>
+           <Input
+             value={this.state.inputValue}
+             placeholder='todo info'
+             style={{width: '300px', marginRight: '10px'}}
+             onChange={this.handleInputChange}>
+           </Input>
+           <Button type="primary" onClick={this.handleButtonClick}>Submit</Button>
          </div>
          <List
           style={{ marginTop: '10px', marginLeft: '10px', width: '390px' }}
           bordered
-          dataSource={data}
-          renderItem={item => (<List.Item>{item}</List.Item>)}
+          dataSource={this.state.list}
+          renderItem={
+            (item, index) =>
+              (<List.Item onClick={this.handleItemDelete.bind(this, index)}>
+            {item}
+          </List.Item>)}
         />
        </div>
      )
    }
+
+   handleInputChange(e){
+     const action = getInputChangeAction(e.target.value);
+     store.dispatch(action);
+   }
+
+   handleStoreChange(){
+     this.setState(store.getState);
+   }
+
+   handleButtonClick(){
+     const action = getAddItemAction();
+     store.dispatch(action);
+   }
+
+   handleItemDelete(index){
+     const action = getDeleteItemAction(index);
+     store.dispatch(action);
+   }
+
 }
 
 export default TodoList;
