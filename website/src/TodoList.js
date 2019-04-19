95e0b5e28b1508b4d06d4fa7cd32bf75eabdf994
leejoonsung <juncheng.li@ucdconnect.ie>
2019-04-19 23:52:38 +0100

    React-Redux
    19/04/2019 React-Redux exploration

--- website/src/TodoList.js
+++ website/src/TodoList.js
@@ -1,55 +1,26 @@
 import React, { Component } from 'react';
-import 'antd/dist/antd.css';
-import store from './store';
-import { getInitList, getInputChangeAction, getAddItemAction, getDeleteItemAction } from './store/actionCreators'
-import TodoListUI from './TodoListUI';
+import connect from 'react-redux';
 
-class TodoList extends Component{
+class TodoList extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = store.getState();
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleStoreChange = this.handleStoreChange.bind(this);
-    this.handleButtonClick = this.handleButtonClick.bind(this);
-    this.handleItemDelete = this.handleItemDelete.bind(this);
-    store.subscribe(this.handleStoreChange);
+  render() {
+    return (
+      <div>
+        <div>
+          <input value={this.props.inputValue}/>
+          <button>submit</button>
+        </div>
+        <ul>
+          <li>Dell</li>
+        </ul>
+      </div>
+    )
   }
-
-   render() {
-     return <TodoListUI
-                inputValue={this.state.inputValue}
-                list={this.state.list}
-                handleInputChange={this.handleInputChange}
-                handleButtonClick={this.handleButtonClick}
-                handleItemDelete={this.handleItemDelete}
-            />
-   }
-
-   componentDidMount() {
-     const action = getInitList();
-     store.dispatch(action);
-   }
-
-   handleInputChange(e){
-     const action = getInputChangeAction(e.target.value);
-     store.dispatch(action);
-   }
-
-   handleStoreChange(){
-     this.setState(store.getState);
-   }
-
-   handleButtonClick(){
-     const action = getAddItemAction();
-     store.dispatch(action);
-   }
-
-   handleItemDelete(index){
-     const action = getDeleteItemAction(index);
-     store.dispatch(action);
-   }
-
 }
 
-export default TodoList;
+const mapStateToProps = ( state ) => {
+  return {
+    inputValue: state.inputValue
+  }
+}
+export default connect(mapStateToProps, null)(TodoList);
--- website/src/store/index.js
+++ website/src/store/index.js
@@ -1,19 +1,6 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import reducer from './reducer';
-import createSagaMiddleware from 'redux-saga'
-import todoSags from './sagas';
 
-// create the saga middleware
-const sagaMiddleware = createSagaMiddleware();
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE_ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
-const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
-// mount it on the Store
-const store = createStore(
-  reducer,
-  enhancer,
-);
-
-// then run the saga
-sagaMiddleware.run(todoSags)
+const store = createStore(reducer);
 
 export default store;
